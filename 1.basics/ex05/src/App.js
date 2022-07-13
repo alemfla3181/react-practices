f13667ece6d62f2fc881aaec829828cbdc6846e3
Dragon <alemfla3181@naver.com>
2022-07-13 17:07:32 +0900

    react api

--- 1.basics/ex05/src/App.js
+++ 1.basics/ex05/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   // const App = document.createElement('div');
   // App.textContent = "Hello World";
-  const App = React.createElement('div', null, 'Hello World');
+  const App = React.createElement('div', null, 'Hello Worldsssssssssssssssssssssss');
   return App;
 }
 
---
+++ 1.basics/ex06/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  // const App = React.createElement('div', null, 'Hello World');
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export { App };
