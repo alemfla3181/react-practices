73a949e9c1e34077ca566c7d80c1a1c8d97acfba
Dragon <alemfla3181@naver.com>
2022-07-20 11:55:17 +0900

    제어 컴포넌트

--- 2.component/ex03/src/02/TitleBar01.js
+++ 2.component/ex03/src/02/TitleBar01.js
@@ -4,11 +4,18 @@ export default class TitelBar01 extends Component {
   constructor(props) {
     super(props);
     
-    this.no = 104;
+    this.no = 10;
+    this.state = {
+      no: 10
+    };
   }
 
   onClickHandler() {
-    console.log(this);
+    // this.no VS this.state.no
+    this.no++;
+    this.setState({
+      no: this.state.no + 1
+    });
     console.log('TitleBar01 clicked!:' + this.no );
   }
 
@@ -20,6 +27,13 @@ export default class TitelBar01 extends Component {
           cursor: 'pointer'
         }}>
         Function Handler(Class Component)
+        <br />
+        {
+        // this.no VS this.state.no
+        
+          `${this.no} VS ${this.state.no}`
+          
+        }
       </h1>
     )
   }
--- 2.component/ex03/src/02/TitleBar02.js
+++ 2.component/ex03/src/02/TitleBar02.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function TitelBar02() {
-  const onClickHandler = e => {
-    console.log('TitleBar02 Clicked');
-  }
-
+  const [stateNo, setStateNo] = useState(10);
+  
   return (
     <h1
-      onClick={onClickHandler}
+      onClick={e => {
+        setStateNo(stateNo + 1)
+        console.log('TitleBar02 Clicked')
+      }}
       style={{ cursor: 'pointer' }}>
       Function Handler(Functional Component)
     </h1>
