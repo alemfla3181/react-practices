dd3299ccad1613cf03a3493949bb82af11bab91b
Dragon <alemfla3181@naver.com>
2022-07-28 13:36:18 +0900

    fetch api

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,27 +1,110 @@
-import React, { useState } from 'react';
-import './assets/scss/App.scss'
-import data from './assets/json/data.json'
-import Emaillist from './Emaillist';
-import Searchbar from './Searchbar';
+import React, {useState, useEffect} from 'react';
 import RegisterForm from './RegisterForm';
+import SearchBar from './SearchBar';
+import Emaillist from './Emaillist';
+import './assets/scss/App.scss';
 
 const App = () => {
-  const [emails, setEmails] = useState(data);
+  const [emails, setEmails] = useState([]);
 
-  const notifyEmailAdd = (email) => {
-    console.log(email)
+  const emailDelete = function (no) {
+    console.log(no);
   }
 
-  const notifyKeywordChanged = (keyword) => {
-    console.log(keyword);
+  const emailAdd = async function (email) {
+    try {
+      const response = await fetch(`/api`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(email)
+      });
+
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const json = await response.json();
+
+      if (json.result !== 'success') {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+
+      setEmails([json.data, ...emails]);
+    } catch (err) {
+      console.log(err);
+    }
   }
+
+  const keywordChanged = async function (keyword) {
+    try {
+      const response = await fetch(`/api?kw=${keyword}`, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: null
+      });
+
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const json = await response.json();
+
+      if (json.result !== 'success') {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+
+      setEmails(json.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api', {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: null
+        });
+
+        if (!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const json = await response.json();
+
+        if (json.result !== 'success') {
+          throw new Error(`${json.result} ${json.message}`);
+        }
+
+        setEmails(json.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
+    
+  }, []);
+
   return (
     <div id={'App'}>
-      <RegisterForm/>
-      <Searchbar callback={notifyKeywordChanged} />
+      <RegisterForm callback={emailAdd} />
+      <SearchBar callback={keywordChanged} />
       <Emaillist emails={emails} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ emaillist/readme.md
@@ -0,0 +1,8 @@
+# React Practices - Integration (개발통합환경)
+## emaillist
+  - 개발통합 환경 설정 (with Spring Boot BE)
+  - Backend: APIs
+  - Frontend: React(SPA)
+  - AJAX: fetch
+
+#### Configuration
